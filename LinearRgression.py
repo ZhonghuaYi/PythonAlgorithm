f97d3c1d992a70636d907999d9856120d380815a
Zhonghua Yi <yizhonghua6@gmail.com>
2021-10-24 16:15:52 +0800

    feat: 16:15, 2021/10/24

--- LinearRgression.py
+++ LinearRgression.py
@@ -1,9 +1,25 @@
-import numpy as np
+import random
+import torch
+from matplotlib import pyplot as plt
+from mpl_toolkits import mplot3d
 
-# 计算代价函数
-# X包含有一列1,因为theta中有一个偏置项
-def lost(X, y, theta):
-    m = len(y)
-    J = 0
-    J = (np.transpose(X * theta - y) * (X * theta - y)) / (2 * m)
-    return J
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+
+def synthetic_data(w, b, num_examples):
+    """生成 y = Xw + b ＋ 噪声"""
+    X = torch.normal(0 ,1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.01, y.shape)
+    return X, y.reshape((-1, 1))
+
+
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = synthetic_data(true_w, true_b, 1000)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter3D(features[:, 0].detach().numpy(), features[:, 1].detach().numpy(), labels.detach().numpy())
+plt.show()
