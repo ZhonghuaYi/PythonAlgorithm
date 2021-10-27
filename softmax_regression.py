665bd17c5457852249c048dd59a50dcb92fe929c
ZhonghuaYi <yizhonghua6@gmail.com>
2021-10-28 01:30:15 +0800

    feat: 1:25, 2021/10/28

--- softmax_regression.py
+++ softmax_regression.py
@@ -3,13 +3,23 @@ import torchvision
 from torch.utils import data
 from torchvision import transforms
 
-trans = transforms.ToTensor()
-mnist_train = torchvision.datasets.FashionMNIST(
-    root='data', train=True, transform=trans, download=False
-)
 
-minst_test = torchvision.datasets.FashionMNIST(
-    root='data', train=False, transform=trans, download=False
-)
+# 返回对应于一个数字或数字序列的文本标签
+def get_fashion_mnist_labels(labels):
+    text_labels = ['t-shit', 'trouser', 'pullover', 'dress', 'coat', 
+                    'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
 
-print(len(mnist_train), len(minst_test))
+
+if __name__ == '__main__':
+
+    trans = transforms.ToTensor()
+
+    # 读取FashionMNIST数据集，download表示是否需要下载数据集
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root='data', train=True, transform=trans, download=False
+    )
+    minst_test = torchvision.datasets.FashionMNIST(
+        root='data', train=False, transform=trans, download=False
+    )
+
